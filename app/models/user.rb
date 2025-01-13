413c6bdb4ae2d724936e42d5b7ead1d20b7330ff
vinny <vini_masotti@hotmail.com>
2025-01-13 23:38:31 +0000

    small ui changes

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+         :password_archivable
+
          validates :password, presence: true, length: { minimum: 8 }, password_complexity: true, if: :password_required?
          
 
