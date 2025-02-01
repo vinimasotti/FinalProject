af79c2478cae9eb5953300cfc5bbf13fac5fa650
vinny <vini_masotti@hotmail.com>
2025-02-01 22:06:29 +0000

    300 char max comment settled

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,13 @@
 module ApplicationHelper
+    #secury measure to not overflood the database
+    def truncate_comment(comment_text, max_length = 300)
+        return "" if comment_text.nil?
+    
+        if comment_text.length > max_length
+          "#{comment_text[0...max_length]}..."
+        else
+          comment_text
+        end
+      end
   
 end
